0290c1cf4943c7206dfc8329b8b54f314f2150e3
Sergei Prutsakov <psn206@mail.ru>
2024-05-03 08:14:35 +0300

    Добавьте файлы проекта.

---
+++ Task/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class BankAccount<T>
+    {
+        T number;
+        double balance;
+        string ownerIsName;
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(T number, double balance, string ownerIsName)
+        {
+            this.Number = number;
+            this.Balance = balance;
+            this.ownerIsName = ownerIsName;
+        }
+
+        public T Number { get => number; set => number = value; }
+        public double Balance { get => balance; set => balance = value; }
+        public string OwnerIsName { get => ownerIsName; set => ownerIsName = value; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Номер счета: {Number}\nБаланс: {Balance}\nФИО: {OwnerIsName}");
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введиет ФИО");
+            OwnerIsName = Console.ReadLine();
+        }
+
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            bankAccount1.GetInfo();
+            bankAccount2.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
